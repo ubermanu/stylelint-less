221f01069c4deb42a4de0d2c707465a7a038e955
ubermanu <manu@paca.happy-dev.fr>
2023-07-22 16:01:47 +0200

    feat: fix selector class pattern against mixins

--- src/rules/index.js
+++ src/rules/index.js
@@ -2,10 +2,12 @@ import colorNoHex from './color-no-hex';
 import colorNoInvalidHex from './color-no-invalid-hex';
 import colorHexCase from './color-hex-case';
 import noDuplicateVariables from './no-duplicate-variables';
+import selectorClassPattern from './selector-class-pattern';
 
 export default {
 	'color-no-hex': colorNoHex,
 	'color-no-invalid-hex': colorNoInvalidHex,
 	'no-duplicate-variables': noDuplicateVariables,
 	'color-hex-case': colorHexCase,
+	'selector-class-pattern': selectorClassPattern,
 };
